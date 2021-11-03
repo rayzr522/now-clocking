21f518be6c01f95b5709bacd2d9a9e644a55f2bd
Batku <32791246+Batku@users.noreply.github.com>
2021-11-03 16:11:57 +0200

    Make start.sh check requirements
    Make start.sh check requirements and tell you what you're missing for easier debugging
    (should only slow it down by at most 10ms)

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #see if you have all necessary rquirements
-command -v conky > /dev/null 2>&1 || { echo >&2 "I require conky but it's not installed. Install it. Aborting."; exit 1; }
-command -v ffmpeg > /dev/null 2>&1 || { echo >&2 "I require ffmpeg but it's not installed. Install it. Aborting."; exit 1; }
-command -v playerctl > /dev/null 2>&1 || { echo >&2 "I require playerctl but it's not installed. Install it. Aborting."; exit 1; }
-command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+command -v conky > /dev/null 2>&1 || { echo >&2 "I require conky but it's not installed.Please install it. Aborting."; exit 1; }
+command -v ffmpeg > /dev/null 2>&1 || { echo >&2 "I require ffmpeg but it's not installed.Please install it. Aborting."; exit 1; }
+command -v playerctl > /dev/null 2>&1 || { echo >&2 "I require playerctl but it's not installed.Please install it. Aborting."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed.Please install it. Aborting."; exit 1; }
 
 set -ex
 
