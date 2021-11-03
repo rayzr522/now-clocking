3d247fb22913a830c1132b48e3ad9977bf174428
Batku <32791246+Batku@users.noreply.github.com>
2021-11-03 15:54:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #see if you have all necessary rquirements
-command -v conky > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
-command -v ffmpeg > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
-command -v playertl > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
+command -v conky > /dev/null 2>&1 || { echo >&2 "I require conky but it's not installed. Install it. Aborting."; exit 1; }
+command -v ffmpeg > /dev/null 2>&1 || { echo >&2 "I require ffmpeg but it's not installed. Install it. Aborting."; exit 1; }
+command -v playerctl > /dev/null 2>&1 || { echo >&2 "I require playerctl but it's not installed. Install it. Aborting."; exit 1; }
 command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
 
 set -ex
